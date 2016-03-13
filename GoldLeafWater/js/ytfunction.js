2e23c0a05771e833f861b1705ab89dcd932f5bf6
Yu Ting <manner_yue@hotmail.com>
2016-03-13 19:51:24 +0800

    3.13commit
    3.13

---
+++ GoldLeafWater/js/ytfunction.js
@@ -0,0 +1,19 @@
+/**
+ * Created by amyaya on 3/13/16.
+ */
+$("#benefits ul li a").mousemove(function(){
+    var c=$(this).parent().attr("data-panel");
+        $("#benefits li").removeClass("active");
+        $("#benefits .copy div").removeClass("active");
+        $(this).parent().addClass("active");
+        $(this).parent().parent().addClass("active");
+        $('.copy div[data-panel="'+c+'"]').addClass("active");
+    var c=$(this).offset().left-15;
+        d=c+$(this).width()/2-37;
+        $("#benefits .arrow").css("background-position",d+"px center");
+})
+/*function(b){
+    var c=b.parent().offset().left-a(".features.tablet").offset().left-15,
+        d=c+b.width()/2-22;
+    a(".arrow").css("background-position",d+"px center")
+}*/
