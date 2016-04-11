5671b8901dff22d24d59d8baf5f5b4780188f6c5
Yu Ting <manner_yue@hotmail.com>
2016-04-11 22:34:28 +0800

    4.10submit
    4.10

--- GoldLeafWater/js/ytfunction.js
+++ GoldLeafWater/js/ytfunction.js
@@ -1,31 +1,24 @@
 /**
  * Created by amyaya on 3/13/16.
  */
-
-
-$("#benefits ul li a").mousemove(function(){
-    var c=$(this).parent().attr("data-panel");
-    $("#benefits li").removeClass("active");
-    $("#benefits .copy div").removeClass("active");
-    $(this).parent().addClass("active");
-    $(this).parent().parent().addClass("active");
-    $('.copy div[data-panel="'+c+'"]').addClass("active");
-    var c=$(this).offset().left-15;
-    d=c+$(this).width()/2-37;
-    $("#benefits .arrow").css("background-position",d+"px center");
-})
-
-function bottleRemove(){
+$(document).ready(function(){
     $("#water").animate({right:'+=220px',opacity:'1'},"slow");
-    //$("#text1").fadeIn(3000);
-    //$("#text2").fadeIn(3000);
-}
+    $(".slogan").hide().fadeIn(2500);
+    $("#text1").hide().delay(2000).slideDown(500);
 
+    var b=$("#init").width()/2-15;
+    $("#benefits .arrow").css("background-position",b+"px center");
 
+    $("#benefits ul li a").mousemove(function(){
+        var c=$(this).parent().attr("data-panel");
+        $("#benefits li").removeClass("active");
+        $("#benefits .copy div").removeClass("active");
+        $(this).parent().addClass("active");
+        $(this).parent().parent().addClass("active");
+        $('.copy div[data-panel="'+c+'"]').addClass("active");
+        var d=$(this).offset().left-$("#benefits").offset().left-15;
+        var e=d+$(this).width()/2-15;
+        $("#benefits .arrow").css("background-position",e+"px center");
+    })
+});
 
-
-/*function(b){
-    var c=b.parent().offset().left-a(".features.tablet").offset().left-15,
-        d=c+b.width()/2-22;
-    a(".arrow").css("background-position",d+"px center")
-}*/
